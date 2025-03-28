a1ca78ae3183eaf0691e939afe9b3da954647123
JackBiller <jackbiller19@gmail.com>
2025-03-28 01:03:59 -0300

    Renderiza a cobrinha em S na tela de shop

--- code/menu.py
+++ code/menu.py
@@ -486,9 +486,40 @@ class Menu:
                 pygame.draw.rect(self.tela, CINZA, (x, y, 300, 150), 2)
                 
                 # Carrega e desenha a imagem da cobrinha
-                imagem_cobra = pygame.image.load(f'assets/{skin[2]}')
-                imagem_cobra = pygame.transform.scale(imagem_cobra, (100, 100))
-                self.tela.blit(imagem_cobra, (x + 20, y + 25))
+                sprite_sheet = pygame.image.load(f'assets/{skin[2]}')
+                sprites_cobra = []
+                for i in range(7):
+                    sprite = sprite_sheet.subsurface((0, i * SPRITE_SIZE, SPRITE_SIZE, SPRITE_SIZE))
+                    sprite = pygame.transform.scale(sprite, (TAMANHO_BLOCO, TAMANHO_BLOCO))
+                    sprites_cobra.append(sprite)
+
+                # Posição inicial ajustada para a área jogável
+                posicao1 = (x + 20 + (TAMANHO_BLOCO * 3), y + 0 + (MARGEM_SUPERIOR /2))
+                posicao2 = (x + 20 + (TAMANHO_BLOCO * 2), y + 0 + (MARGEM_SUPERIOR /2))
+                posicao3 = (x + 20 + (TAMANHO_BLOCO * 1), y + 0 + (MARGEM_SUPERIOR /2))
+                posicao4 = (x + 20 + (TAMANHO_BLOCO * 1), y + (TAMANHO_BLOCO * 1) + (MARGEM_SUPERIOR /2))
+                posicao5 = (x + 20 + (TAMANHO_BLOCO * 1), y + (TAMANHO_BLOCO * 2) + (MARGEM_SUPERIOR /2))
+                posicao6 = (x + 20 + (TAMANHO_BLOCO * 2), y + (TAMANHO_BLOCO * 2) + (MARGEM_SUPERIOR /2))
+                posicao7 = (x + 20 + (TAMANHO_BLOCO * 3), y + (TAMANHO_BLOCO * 2) + (MARGEM_SUPERIOR /2))
+                posicao8 = (x + 20 + (TAMANHO_BLOCO * 3), y + (TAMANHO_BLOCO * 3) + (MARGEM_SUPERIOR /2))
+                posicao9 = (x + 20 + (TAMANHO_BLOCO * 3), y + (TAMANHO_BLOCO * 4) + (MARGEM_SUPERIOR /2))
+                posicao10 = (x + 20 + (TAMANHO_BLOCO * 2), y + (TAMANHO_BLOCO * 4) + (MARGEM_SUPERIOR /2))
+                posicao11 = (x + 20 + (TAMANHO_BLOCO * 1), y + (TAMANHO_BLOCO * 4) + (MARGEM_SUPERIOR /2))
+                self.posicao = [posicao1, posicao2, posicao3, posicao4, posicao5, posicao6, posicao7, posicao8, posicao9, posicao10, posicao11]
+                self.direcao = [TAMANHO_BLOCO, 0]
+                self.comprimento = 3
+                self.sprites = sprites_cobra
+                self.rotacoes = {
+                    (0, -TAMANHO_BLOCO): 0,  # Cima
+                    (0, TAMANHO_BLOCO): 180,  # Baixo
+                    (-TAMANHO_BLOCO, 0): 90,  # Esquerda
+                    (TAMANHO_BLOCO, 0): 270   # Direita
+                }
+                for i, posicao in enumerate(self.posicao):
+                    sprite = self.obter_sprite_parte(i)
+                    self.tela.blit(sprite, posicao)
+                # imagem_cobra = pygame.transform.scale(imagem_cobra, (100, 100))
+                # self.tela.blit(imagem_cobra, (x + 20, y + 25))
                 
                 # Desenha o nome da skin
                 texto_nome = self.fonte.render(skin[1], True, BRANCO)
@@ -625,3 +656,67 @@ class Menu:
         
         # Aguarda um momento
         pygame.time.wait(2000) 
+
+    def obter_sprite_parte(self, indice):
+        if indice == 0:  # Cabeça
+            if self.direcao == [0, TAMANHO_BLOCO]:  # Baixo
+                return self.sprites[CABECA_BAIXO]
+            elif self.direcao == [-TAMANHO_BLOCO, 0]:  # Esquerda
+                return self.sprites[CABECA_ESQUERDA]
+            elif self.direcao == [TAMANHO_BLOCO, 0]:  # Direita
+                return pygame.transform.flip(self.sprites[CABECA_ESQUERDA], True, False)
+            else:  # Cima
+                sprite_cabeça = pygame.transform.rotate(self.sprites[CABECA_BAIXO], 180)
+                sprite_cabeça = pygame.transform.flip(sprite_cabeça, True, False)
+                return sprite_cabeça
+            
+                    
+        elif indice == len(self.posicao) - 1:  # Rabo
+            dx = self.posicao[-1][0] - self.posicao[-2][0]
+            dy = self.posicao[-1][1] - self.posicao[-2][1]
+            
+            if dx != 0:  # Movimento horizontal
+                sprite_rabo = self.sprites[RABO_LATERAL]
+                # Se o rabo estiver indo da direita para esquerda, gira 180°
+                if dx > 0:
+                    sprite_rabo = pygame.transform.rotate(sprite_rabo, 180)
+                return sprite_rabo
+            else:  # Movimento vertical
+                sprite_rabo = self.sprites[RABO_VERTICAL]
+                # Se o rabo estiver indo de baixo para cima, gira 180°
+                if dy > 0:
+                    sprite_rabo = pygame.transform.rotate(sprite_rabo, 180)
+                    sprite_rabo = pygame.transform.flip(sprite_rabo, True, False)
+                return sprite_rabo
+
+        else:  # Corpo
+            prev = self.posicao[indice-1]
+            curr = self.posicao[indice]
+            next = self.posicao[indice+1]
+            
+            dx1 = curr[0] - prev[0]
+            dy1 = curr[1] - prev[1]
+            dx2 = next[0] - curr[0]
+            dy2 = next[1] - curr[1]
+            
+            if (dx1 != 0 and dx2 != 0) or (dy1 != 0 and dy2 != 0):
+                # Movimento em linha reta
+                if dx1 != 0:
+                    return self.sprites[CORPO_HORIZONTAL]
+                else:
+                    return self.sprites[CORPO_VERTICAL]
+            else:
+                # Curva
+                sprite_curva = self.sprites[CORPO_CURVA_LADO_BAIXO]
+                # Da esquerda para baixo (padrão)
+                if (dx1 > 0 and dy2 > 0) or (dy1 < 0 and dx2 < 0):
+                    return sprite_curva
+                # Da direita para baixo
+                elif (dx1 < 0 and dy2 > 0) or (dy1 < 0 and dx2 > 0):
+                    return pygame.transform.flip(sprite_curva, True, False)
+                # Da esquerda para cima
+                elif (dx1 > 0 and dy2 < 0) or (dy1 > 0 and dx2 < 0):
+                    return pygame.transform.flip(sprite_curva, False, True)
+                # Da direita para cima
+                else:
+                    return pygame.transform.flip(sprite_curva, True, True)
